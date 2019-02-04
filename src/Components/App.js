ff0c53c878a00faf2c870ac6f2f7e02698c56134
ryanboris <ryan.boris@gmail.com>
2019-02-04 18:12:43 -0500

    ✨ adds usecontext to pass props between components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { UserContext } from '../index';
 
 export default function App() {
-  return <></>;
+  return (
+    <div>
+      <UserContext.Consumer>{value => <div>{value}</div>}</UserContext.Consumer>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
+/* eslint-disable import/prefer-default-export */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export const UserContext = React.createContext({});
 
-if (module.hot) {
-  module.hot.accept();
-}
+const user = 'test context';
+
+ReactDOM.render(
+  <UserContext.Provider value={user}>
+    <App />
+  </UserContext.Provider>,
+  document.getElementById('root')
+);
