c44cc3c28a393b2d6bc586d2068beda64ac6396d
ryanboris <ryan.boris@gmail.com>
2019-02-05 00:49:21 -0500

    💄 applies styles

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,40 @@
 import React, { useContext } from 'react';
 import TodosContext from '../context';
 
+import TodoStyles from './TodoStyles';
+
 export default function TodoList() {
   const { state, dispatch } = useContext(TodosContext);
+  const title =
+    state.todos.length > 0 ? `${state.todos.length} Todos` : 'Nothing to do!';
 
   return (
-    <div>
-      <ul>
+    <TodoStyles className='container mx-auto max-w-md text-center font-mono'>
+      <h1 className='text-bold'>{title}</h1>
+      <ul className='list-reset text-white p-0'>
         {state.todos.map(todo => (
-          <li key={todo.id}>
-            <span>{todo.text}</span>
+          <li
+            key={todo.id}
+            className='flex items-center bg-orange-dark border-black border-dotted border-2 my-2 py-4'
+          >
+            <span className='cursor-pointer flex-1 ml-12'>{todo.text}</span>
+            <button type='button'>
+              <img
+                src='https://icon.now.sh/edit/ffffff'
+                alt='edit icon'
+                className='h-10 p-1'
+              />
+            </button>
+            <button type='button'>
+              <img
+                src='https://icon.now.sh/delete/000000'
+                alt='delete icon'
+                className='h-8 mx-2'
+              />
+            </button>
           </li>
         ))}
       </ul>
-    </div>
+    </TodoStyles>
   );
 }
---
+++ src/components/TodoStyles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const TodoStyles = styled.div`
+  h1 {
+    font-size: 4rem;
+    letter-spacing: 4px;
+    padding: 20px 0 50px 0;
+  }
+`;
+
+export { TodoStyles as default };
